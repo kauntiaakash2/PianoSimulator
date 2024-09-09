8d792449bf79c15e74372c0ed2271c9212dcd7de
Akash Agarwal <akashkauntia2006@gmail.com>
2024-09-09 10:55:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-// Get all the keys
 const keys = document.querySelectorAll('.key');
-
-// Add event listener to each key
 keys.forEach(key => {
     key.addEventListener('click', () => {
-        // Get the note of the key
         const note = key.getAttribute('data-note');
-
-        // Play the note
         playNote(note);
     });
 });
-
-// Function to play a note
 function playNote(note) {
-    // Create a new audio context
     const audioContext = new AudioContext();
-
-    // Create an oscillator
     const oscillator = audioContext.createOscillator();
-
-    // Set the frequency of the oscillator based on the note
     const frequencies = {
         'C4': 261.63,
         'D4': 293.66,
@@ -32,14 +19,8 @@ function playNote(note) {
     };
     oscillator.type = 'sine';
     oscillator.frequency.value = frequencies[note];
-
-    // Connect the oscillator to the destination
     oscillator.connect(audioContext.destination);
-
-    // Start the oscillator
     oscillator.start();
-
-    // Stop the oscillator after 1 second
     setTimeout(() => {
         oscillator.stop();
     }, 1000);
